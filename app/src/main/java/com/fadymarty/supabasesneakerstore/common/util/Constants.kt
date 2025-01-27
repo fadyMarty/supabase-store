ae827e0794317cf2d3dd463ac3be1c08163e80fc
fadyMarty <antoshka1234098@gmail.com>
2025-01-27 15:27:28 +0300

    Commit

--- app/src/main/java/com/fadymarty/supabasesneakerstore/common/util/Constants.kt
+++ app/src/main/java/com/fadymarty/supabasesneakerstore/common/util/Constants.kt
@@ -2,8 +2,8 @@ package com.fadymarty.supabasesneakerstore.common.util
 
 object Constants {
 
-    const val SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRjbWlpa2p5dXlrbnN2b2J6d2RoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc3ODEwNTYsImV4cCI6MjA1MzM1NzA1Nn0.E59-XILWMmria_TK-itERTm6FDbeP3TasVg6pyHE0sY"
-    const val SUPABASE_URL = "https://tcmiikjyuyknsvobzwdh.supabase.co"
+    const val SUPABASE_KEY = "*"
+    const val SUPABASE_URL = "*"
 
     const val USER_SETTINGS = "user_settings"
     const val APP_ENTRY = "app_entry"
